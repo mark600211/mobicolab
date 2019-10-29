fbc4d6c42af259575574365a2991b36758208980
markMint <markoffka11@me.com>
2019-10-30 06:29:00 +0900

    Create Navbar

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,44 +1,44 @@
-import React, { Component } from 'react'
-import { Container, Media } from 'reactstrap'
-import Jumbotron from 'reactstrap/lib/Jumbotron'
+import React, { Component } from 'react';
+import { Container, Media } from 'reactstrap';
+import Jumbotron from 'reactstrap/lib/Jumbotron';
+import NavbarComponent from './NavbarComponent';
 
 export default class HeaderComponent extends Component {
     render() {
         return (
             <div>
                 <Jumbotron className = "header_background">
-                    <Container>
-                        <Media>
-                            <Media left top href="#">
-                                <Media object src="assets/images/logo.pdf" alt="Logo" />
+                    <Media>
+                        <Media left top href="#">
+                            <Media object src="assets/images/list_logo.png" alt="Logo" />
+                        </Media>
+                        <Media body>
+                            <Media heading>
+                                <h1>Мобильная<br/>
+                                    Экологическая<br/>
+                                    Лаборатория
+                                </h1>
                             </Media>
-                            <Media body>
-                                <Media heading>
-                                    <h1>Мобильная<br/>
-                                        Экологическая<br/>
-                                        Лаборатория
-                                    </h1>
+                            <Media>
+                                <Media left top>
+                                    <Media className="header_list" object src="assets/images/list_logo.png" />  
                                 </Media>
-                                <Media>
-                                    <Media left top>
-                                        <Media className="header_list" object src="assets/images/list_logo.png" />  
-                                    </Media>
-                                    <Media body>
-                                        Лабораторно-аналитические исследования компонентов окружающей среды
-                                    </Media>
+                                <Media body>
+                                    Лабораторно-аналитические исследования компонентов окружающей среды
                                 </Media>
-                                <Media>
-                                    <Media left middle>
-                                        <Media className="header_list" object src="assets/images/list_logo1.png" />  
-                                    </Media>
-                                    <Media body>
-                                        Наблюдение за морфометрическими особенностями и состоянием водоохранных зон водных объектов
-                                    </Media>
+                            </Media>
+                            <Media>
+                                <Media left middle>
+                                    <Media className="header_list" object src="assets/images/list_logo1.png" />  
+                                </Media>
+                                <Media body>
+                                    Наблюдение за морфометрическими особенностями и состоянием водоохранных зон водных объектов
                                 </Media>
                             </Media>
                         </Media>
-                    </Container>
+                    </Media>
                 </Jumbotron>
+                <NavbarComponent />
             </div>
         )
     }
---
+++ src/components/NavbarComponent.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    UncontrolledDropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem } from 'reactstrap';
+
+
+
+class NavbarComponent extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.toggleNav = this.toggleNav.bind(this);
+        this.state = {
+            isNavOpen: false,
+        }
+    }
+
+    toggleNav() {
+        this.setState({
+            isNavOpen: !this.state.isNavOpen
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <Navbar color="dark" dark expand="md">
+                    <NavbarBrand href="/">
+                        <img className="logo_navbar" object src="assets/images/list_logo.png" alt="Logo" />
+                    </NavbarBrand>
+                    <NavbarToggler onClick={this.toggleNav} />
+                    <Collapse isOpen={this.state.isNavOpen} navbar>
+                        <Nav navbar>
+                            <NavItem>
+                                <NavLink href="#">О нас</NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink href="#">Наши партнёры</NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink href="#">Контакты</NavLink>
+                            </NavItem>
+                         </Nav>
+                     </Collapse>
+                 </Navbar>
+            </div>
+        );
+    }
+}
+
+export default NavbarComponent;
